0d6f7298182a6873926932deee1f7426eefa2e7b
leviatas <31702513+leviatas@users.noreply.github.com>
2017-09-15 10:07:10 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var app = express();
+app.set('port', (process.env.PORT || 5000));
+app.get('/', function(request, response) {
+  
+});
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
+
+const Telegram = require('telegram-node-bot')
+const TelegramBaseController = Telegram.TelegramBaseController
+const tg = new Telegram.Telegram('286347105:AAEST1sg39bF1pVMcrF_klijfMuTlkORP-U')
+class GreetingController extends TelegramBaseController {
+    /**
+     * @param {Scope} $
+     */
+    greetingHandler($) {
+        $.sendMessage('Hey, how are you?')
+    }
+get routes() {
+        return {
+            'hey': 'greetingHandler',
+            'hi': 'greetingHandler',
+            'hello': 'greetingHandler',
+        }
+    }
+}
+tg.router
+  .when(['hey', 'hi', 'hello'], new GreetingController())
