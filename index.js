ad1af6b5b80fe519f48bf1a550d2bb48caa31463
Peluffo Eduardo <epeluffo@austral.edu.ar>
2017-09-18 16:23:12 -0300

    Se hacen cambios al telegram

--- index.js
+++ index.js
@@ -9,10 +9,9 @@ app.get('/', function(request, response) {
 
 app.listen(process.env.PORT || 3000);
 
-var TelegramBot = require('node-telegram-bot-api'),
-    // Be sure to replace YOUR_BOT_TOKEN with your actual bot token on this line.
-    telegram = new TelegramBot("286347105:AAEST1sg39bF1pVMcrF_klijfMuTlkORP-U", { polling: true });
-
+const Telegram = require('telegram-node-bot')
+const TelegramBaseController = Telegram.TelegramBaseController
+const tg = new Telegram.Telegram('286347105:AAEST1sg39bF1pVMcrF_klijfMuTlkORP-U')
 
 class GreetingController extends TelegramBaseController {
     /**
