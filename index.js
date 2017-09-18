8afd37b43b6e34e01407b87a7a3f7fd4ebbe1a41
Peluffo Eduardo <epeluffo@austral.edu.ar>
2017-09-18 15:46:39 -0300

    Se elimina comentarios, se quita engines

--- index.js
+++ index.js
@@ -2,10 +2,6 @@ var TelegramBot = require('node-telegram-bot-api'),
     // Be sure to replace YOUR_BOT_TOKEN with your actual bot token on this line.
     telegram = new TelegramBot("286347105:AAEST1sg39bF1pVMcrF_klijfMuTlkORP-U", { polling: true });
 
-//telegram.on("text", (message) => {
-//  telegram.sendMessage(message.chat.id, "Hello world");
-//});
-
 var Clear = require('codeday-clear'),
     // Our sample app token and secret
     clear = new Clear("1YZiGaj3baaLU8IKVsASRIWaNF2oJNg0", "1COMnWyGnGBsNqkhaZ6WMBWB9UWZw6QZ");
