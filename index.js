bb92b248a8f099db5b134b2e186d56f81057f7f7
Peluffo Eduardo <epeluffo@austral.edu.ar>
2017-09-18 17:28:58 -0300

    Se agrega wek hook

--- index.js
+++ index.js
@@ -9,10 +9,60 @@ app.get('/', function(request, response) {
 
 app.listen(process.env.PORT || 3000)
 
-var TelegramBot = require('node-telegram-bot-api'),
-    // Be sure to replace YOUR_BOT_TOKEN with your actual bot token on this line.
-    telegram = new TelegramBot("286347105:AAEST1sg39bF1pVMcrF_klijfMuTlkORP-U", { polling: true });
 
+const TOKEN = process.env.TELEGRAM_TOKEN || '286347105:AAEST1sg39bF1pVMcrF_klijfMuTlkORP-U';
+
+var TelegramBot = require('node-telegram-bot-api');
+
+const options = {
+   webHook: {
+    // Port to which you should bind is assigned to $PORT variable
+    // See: https://devcenter.heroku.com/articles/dynos#local-environment-variables
+    port: process.env.PORT
+    // you do NOT need to set up certificates since Heroku provides
+    // the SSL certs already (https://<app-name>.herokuapp.com)
+    // Also no need to pass IP because on Heroku you need to bind to 0.0.0.0
+  }
+
+}
+
+
+// Heroku routes from port :443 to $PORT
+// Add URL of your app to env variable or enable Dyno Metadata
+// to get this automatically
+// See: https://devcenter.heroku.com/articles/dyno-metadata
+const url = process.env.APP_URL || 'https://leviatasbot.herokuapp.com:443';
+const bot = new TelegramBot(TOKEN, options);
+
+// This informs the Telegram servers of the new webhook.
+// Note: we do not need to pass in the cert, as it already provided
+bot.setWebHook(`${url}/bot${TOKEN}`);
+
+
+// Just to ping!
+bot.on('message', function onMessage(msg) {
+  bot.sendMessage(msg.chat.id, 'I am alive on Heroku!');
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
 var Clear = require('codeday-clear'),
     // Our sample app token and secret
     clear = new Clear("1YZiGaj3baaLU8IKVsASRIWaNF2oJNg0", "1COMnWyGnGBsNqkhaZ6WMBWB9UWZw6QZ");
@@ -60,3 +110,4 @@ telegram.on("inline_query", (query) => {
     telegram.answerInlineQuery(query.id, queryResults);
   });
 });
+*/
