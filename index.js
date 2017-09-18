c0a206a9ba0818c536e6fd3a881fa5d30ab9f9aa
Peluffo Eduardo <epeluffo@austral.edu.ar>
2017-09-18 15:56:12 -0300

    Change port

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+app.set('port', process.env.PORT || 3000);
+
 var TelegramBot = require('node-telegram-bot-api'),
     // Be sure to replace YOUR_BOT_TOKEN with your actual bot token on this line.
     telegram = new TelegramBot("286347105:AAEST1sg39bF1pVMcrF_klijfMuTlkORP-U", { polling: true });
