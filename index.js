3232b02e8210862b68693519c78f7febb92d2e08
Peluffo Eduardo <epeluffo@austral.edu.ar>
2017-09-18 17:39:33 -0300

    Quito el express.

--- index.js
+++ index.js
@@ -1,15 +1,3 @@
-var express = require('express');
-var app = express();
-
-app.set('port', process.env.PORT || 3000);
-
-app.get('/', function(request, response) {
-  
-});
-
-app.listen(process.env.PORT || 3000)
-
-
 const TOKEN = process.env.TELEGRAM_TOKEN || '286347105:AAEST1sg39bF1pVMcrF_klijfMuTlkORP-U';
 var TelegramBot = require('node-telegram-bot-api');
 const options = {
