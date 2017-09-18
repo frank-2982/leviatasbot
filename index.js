1f28eeb1c3144da02dd525adf207345a3f43cd25
Peluffo Eduardo <epeluffo@austral.edu.ar>
2017-09-18 12:22:27 -0300

    Se actualiza

--- index.js
+++ index.js
@@ -14,5 +14,39 @@ var Clear = require('codeday-clear'),
 var moment = require('moment');
 
 telegram.on("text", (message) => {
-  telegram.sendMessage(message.chat.id, "Hello world");
+  if(message.text.toLowerCase().indexOf("/codeday") === 0){
+    clear.getEventById("oo4QIuKQQTYA", (codedayEvent) => {
+      var endsAt = moment(codedayEvent.ends_at * 1000);
+      telegram.sendMessage(message.chat.id, "CodeDay ends *" + endsAt.fromNow() + "*!", {
+           parse_mode: "Markdown"
+      });
+    });
+  }
+});
+
+telegram.on("inline_query", (query) => {
+  var searchTerm = query.query.trim();
+
+  clear.getRegions((regions) => {
+    var queryResults = [ ];
+
+    regions.forEach((region) => {
+      if(region.name.toLowerCase().indexOf(searchTerm.toLowerCase()) !== -1 && region.current_event && region.current_event.venue){
+        queryResults.push({
+          type: "article",
+          id: region.id,
+          title: "CodeDay " + region.name,
+          description: "Hosted at " + region.current_event.venue.full_address,
+          input_message_content: {
+            latitude: region.location.lat,
+            longitude: region.location.lng,
+            title: "CodeDay " + region.name,
+            address: region.current_event.venue.full_address
+          }
+        });
+      }
+    });
+
+    telegram.answerInlineQuery(query.id, queryResults);
+  });
 });
